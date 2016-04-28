6725606f81b740c52199ea51139d256bc41cd988
MikhailKovalenko <mikhail.kovalenko96@gmail.com>
2016-04-28 12:11:33 +0300

    Edit TodoServlet.java

--- src/TodoServlet.java
+++ src/TodoServlet.java
@@ -3,6 +3,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,13 +13,28 @@ import java.util.Map;
  */
 public class TodoServlet extends HttpServlet{
 
-    private ArrayList<String> tasks = new ArrayList<>();
+//    private ArrayList<String> tasks = new ArrayList<>();
 
     private  void  outputList(HttpServletResponse resp) throws IOException, ServletException {
+        ArrayList<String> tasks = new ArrayList<>();
+        try (Connection conn = DriverManager.getConnection("jdbc:h2:~/test")) {
+            try (PreparedStatement st = conn.prepareStatement("SELECT ID, TEXT FROM TODO ORDER BY ID DESC")) {
+                try (ResultSet rs = st.executeQuery()) {
+                    while (rs.next()) {
+                        int id = rs.getInt(1);
+                        String text = rs.getString(2);
+                        tasks.add(text);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new ServletException(e);
+        }
         resp.setCharacterEncoding("UTF-8");
         Map<String,Object> data = new HashMap<>();
         data.put("allTasks", tasks);
         TemplateUtil.render("todo.vsl", data, resp.getWriter());
+
     }
 
     @Override
@@ -28,7 +44,7 @@ public class TodoServlet extends HttpServlet{
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String newTask=req.getParameter("newTask");
-        tasks.add(newTask);
+  //      tasks.add(newTask);
         outputList(resp);
     }
 }
